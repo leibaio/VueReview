f5255e2d12a861320d2bb8937010fa5aea38334a
leibaio <leibaio@qq.com>
2022-02-25 00:31:01 +0800

    通过key管理状态

--- 1-简介/vue-demo/src/components/MyComponent.vue
+++ 1-简介/vue-demo/src/components/MyComponent.vue
@@ -29,6 +29,13 @@
         isActive: true,
         hasError: false,
         awesome: true,
+        items: [{ message: 'Foo' }, { message: 'Bar'}],
+        parentMessage: 'Parent',
+        myObject: {
+          title: '如何在Vue中渲染列表',
+          author: '王小明',
+          publishedAt: '2022-02-22'
+        }
       }
     },
 
@@ -120,4 +127,49 @@
   <!-- v-if VS v-show -->
   <!-- v-if是真是的按条件渲染，是懒加载的，如果初次渲染为false，不会做任何事，条件区域在条件首次变为true才渲染 ；
   相比之下，v-show简单许多，无论如何，始终会被渲染，仅做CSS类的切换； 因此，需要频繁切换时，用v-show更好，而不太会改变，用v-if更合适-->
+
+  <!-- 可以使用v-for基于一个数组渲染一个列表。需要一种特殊的语法形式 item in items, items是源数据的数组，而item是对迭代项的别名 -->
+
+  <li v-for="item in items">
+    {{ item.message }}
+  </li>
+
+  <!-- v-for 块中可以完整地访问父作用域内的属性。v-for也支持使用可选的第二个参数，表示当前项的位置索引 -->
+
+  <li v-for="(item, index) in items">
+    {{ parentMessage }} - {{ index }} - {{ item.message }}
+  </li>
+
+  <!-- 也可以使用 v-for 来遍历一个对象的所有属性 -->
+  <ul>
+    <li v-for="value in myObject">
+      {{ value }}
+    </li>
+  </ul>
+
+  <!-- 可以提供第二个参数表示属性名(例如 key) -->
+  <ul>
+    <li v-for="(value, key) in myObject">
+      {{ key }} - {{ value }}
+    </li>
+  </ul>
+
+  <!-- 可以直接给 v-for 传一个整数值 -->
+  <span v-for="n in 10"> {{ n }} </span>
+  <!-- 注意此处的n从1而并非0 -->
+
+  <!-- template上的v-for -->
+  <ul>
+    <template v-for="item in items">
+      <li>{{ item.message }}</li>   
+    </template>
+  </ul>
+
+  <!-- 当Vue更新一个v-for渲染的列表，默认采用“原地修补”的策略。如果数据项顺序发生了改变，Vue不是通过移动DOM元素来匹配条目的顺序，而是在恰当的位置对每个元素进行修补。
+  
+  为了给Vue一个提示，以便它可以跟踪每个节点的标识，从而重用和重新排序现有的元素，需要为每个项目提供一个唯一的key属性
+   -->
+  <div v-for="item in items" :key="item.id">
+    <li>{{ item.message }}</li>
+  </div>
 </template>
