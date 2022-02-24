be5331edb4c9f6cefbedd761f8e57b33c0e7ce58
leibaio <leibaio@qq.com>
2022-02-24 12:34:25 +0800

    vue-demo

---
+++ 1-简介/vue-demo/src/components/MyComponent.vue
@@ -0,0 +1,62 @@
+<script>
+  export default {
+    data() {
+      return {
+        count: 0,
+        msg: 'leibaio',
+        rawHtml: `<span style="color: red">This should be red.</span>`,
+        dynamicId: 44,
+        isButtonDisabled: true,
+        objectOfAttrs: {
+          id: 'container',
+          class: 'wrapper'
+        },
+        number: 45,
+        ok: 1,
+        message: 'petronas',
+        seen: true,
+        url: 'https://leibaio.space',
+      }
+    },
+
+    methods: {
+      increment() {
+        this.count++
+      }
+    },
+
+    mounted() {
+      console.log(`The initial count is ${this.count}.`);
+    }
+  }
+</script>
+
+<template>    
+  <button @click="increment">the count is: {{ count }}</button>
+
+  <br>
+
+  <span>Message: {{ msg }}</span>
+
+  <p>Using text interpolation: {{ rawHtml }}</p>
+
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+
+  <div :id="dynamicId">{{ dynamicId }}</div>
+
+  <button :disabled="isButtonDisabled">Button</button>
+
+  <div :="objectOfAttrs">{{ objectOfAttrs.id }}</div>
+
+  <div>{{ number - 1 }}</div>
+
+  <div>{{ ok ? 'YES' : 'NO' }}</div>
+
+  <div>{{ message.split('').reverse().join('') }}</div>
+
+  <p v-if="seen">Now you see me</p>
+
+  <a :href="url"> ... </a>
+
+  <p @click=""> click me </p>
+</template>
